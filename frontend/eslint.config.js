af7ef94272d5964b5bf0c2f40ffbea8daeab9f3d
Max Mulder <maxmulder03@gmail.com>
2025-04-09 17:17:45 -0400

    TS, Eslint, Prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,36 +3,72 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-plugin-prettier'
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    ignores: ['dist', 'node_modules']
+  },
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
       },
+
+      globals: {
+        ...globals.browser,
+        ...globals.es2022
+      }
     },
-    settings: { react: { version: '18.3' } },
+
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+
     plugins: {
+      '@typescript-eslint': tsPlugin,
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      prettier
     },
+
     rules: {
       ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
+
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      ...jsxA11y.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
+
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto'
+        }
+      ],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  }
 ]
