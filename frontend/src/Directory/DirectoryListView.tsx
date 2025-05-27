70e9c7a0b7778bf072083f566a1b1bb84760038b
Patrick <wagaodongo@gmail.com>
2025-05-27 15:05:39 -0400

    Sticky Column Z-Index Fix

--- frontend/src/Directory/DirectoryListView.tsx
+++ frontend/src/Directory/DirectoryListView.tsx
@@ -1,48 +1,82 @@
 import { User, typeToBadgeClass } from "../types/User";
-import { getGridTemplateColumns } from "../utils/utils";
 
 interface DirectoryListViewProps {
   users: User[];
 }
 
 function DirectoryListView({ users }: DirectoryListViewProps) {
-  const numFields = Object.keys(users[0]).length;
-
-  console.log(getGridTemplateColumns(numFields));
-
   return (
-    <div box-="round" className="overflow-x-auto">
-      <div className={`grid ${getGridTemplateColumns(numFields)} gap-2 p-2`}>
-       <div className="sticky left-0 z-20 px-2">Name</div>
-        <div>Email</div>
-        <div>Github Account</div>
-        <div className="justify-self-center">Employee Type</div>
-        <div>Active</div>
+    <div box-="round" className="overflow-hidden">
+      <div className="overflow-x-auto">
+        <table className="w-full min-w-max table-fixed">
+          <thead>
+            <tr className="whitespace-nowrap">
+              <th className="sticky left-0 z-30 w-48 text-left p-2 border-b font-semibold bg-[var(--background0)]">
+                Name
+              </th>
+              <th className="w-64 text-left p-2 border-b font-semibold">
+                Email
+              </th>
+              <th className="w-40 text-left p-2 border-b font-semibold">
+                Github Account
+              </th>
+              <th className="w-36 text-center p-2 border-b font-semibold">
+                Employee Type
+              </th>
+              <th className="w-24 text-center p-2 border-b font-semibold">
+                Active
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, idx) => (
+              <tr
+                key={user.fullName + idx}
+                className="whitespace-nowrap"
+              >
+                <td className="sticky left-0 z-20 p-2 border-b bg-[var(--background0)]">
+                  <div className="truncate pr-2" title={user.fullName}>
+                    {user.fullName}
+                  </div>
+                </td>
+                <td className="p-2 border-b">
+                  <div
+                    is-="badge"
+                    variant-="background2"
+                    className="inline-block max-w-full"
+                    title={user.email}
+                  >
+                    <span className="truncate block">{user.email}</span>
+                  </div>
+                </td>
+                <td className="p-2 border-b">
+                  <a
+                    href={`https://github.com/${user.githubUsername}`}
+                    className="block truncate hover:underline text-blue-600"
+                    title={user.githubUsername}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {user.githubUsername}
+                  </a>
+                </td>
+                <td className="p-2 border-b text-center">
+                  <div
+                    is-="badge"
+                    variant-={typeToBadgeClass[user.employeeType]}
+                    className="inline-block whitespace-nowrap"
+                  >
+                    {user.employeeType}
+                  </div>
+                </td>
+                <td className="p-2 border-b text-center">
+                  {user.activeEmployee ? "Yes" : "No"}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-
-      {users.map((user, idx) => (
-        <div
-          key={user.fullName + idx}
-          className={`grid ${getGridTemplateColumns(numFields)} gap-2 p-2`}
-        >
-          <div> {user.fullName} </div>
-          <div is-="badge" variant-="background2" className="w-fit overflow-hidden text-ellipsis max-w-full"
-            title={user.email} >
-            <span className="block truncate">{user.email}</span>
-          </div>
-          <a href={`https://github.com/${user.githubUsername}`}>
-            {user.githubUsername}
-          </a>
-          <div
-            is-="badge"
-            variant-={typeToBadgeClass[user.employeeType]}
-            className="w-fit justify-self-center"
-          >
-            {user.employeeType}
-          </div>
-          <div> {user.activeEmployee} </div>
-        </div>
-      ))}
     </div>
   );
 }
