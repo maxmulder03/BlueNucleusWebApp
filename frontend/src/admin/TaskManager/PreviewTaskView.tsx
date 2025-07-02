bc94c7bfb8e7cd2342277a64697c9db857f76b1a
katzmann835 <katzmann@umich.edu>
2025-07-02 14:17:02 -0400

    Minor fixes

--- frontend/src/admin/TaskManager/PreviewTaskView.tsx
+++ frontend/src/admin/TaskManager/PreviewTaskView.tsx
@@ -58,7 +58,7 @@ function ViewTask() {
                     ? "text-[var(--green)] bg-[var(--background0)] button-[var(--background0)] no-underline"
                     : focused === ""
                       ? ""
-                      : "text-[var(--foreground2)]"
+                      : "text-[var(--foreground2)] no-underline"
                 }
               >
                 {" "}
