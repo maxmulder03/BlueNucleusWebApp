593bcbdf0cf7f0891924caf8334db3b866888b51
katzmann835 <katzmann@umich.edu>
2025-07-03 13:20:18 -0400

    Created new css module, when clicked, background border no longer changes

--- frontend/src/admin/TaskManager/PreviewTaskView.tsx
+++ frontend/src/admin/TaskManager/PreviewTaskView.tsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import styles from "./Click.module.css";
+
+const Click = () => {
+  return <div className={styles.header}></div>;
+};
 
 function ViewTask() {
   const [focused, setFocused] = useState<string>("");
